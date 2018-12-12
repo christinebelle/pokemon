ffcc93984b9cee4f114ba5d93f18faad3cd7dfa9
adjasam1 <samuel.sabot.lp2@gmail.com>
2018-12-12 10:53:56 +0100

    class poké

--- src/main/java/projetPokemon/model/PokemonEntity.java
+++ src/main/java/projetPokemon/model/PokemonEntity.java
@@ -1,5 +1,17 @@
 package projetPokemon.model;
 
 public class PokemonEntity {
+	
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer idPok;
+    private String nomPok;
+    private String evol1;
+    private String evol2;
+    
+    @OneToMany(cascade=CascadeType.ALL)
+    private Type type;
+    
+    
 
 }
