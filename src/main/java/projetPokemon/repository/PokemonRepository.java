17e16dbd89f1c834ae5b9c5f247f28973a241aa8
adjasam1 <samuel.sabot.lp2@gmail.com>
2018-12-12 18:42:51 +0100

    ajout methode recherche par nom controleur

--- src/main/java/projetPokemon/repository/PokemonRepository.java
+++ src/main/java/projetPokemon/repository/PokemonRepository.java
@@ -1,9 +1,14 @@
 package projetPokemon.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import projetPokemon.model.PokemonEntity;
 
 public interface PokemonRepository extends CrudRepository<PokemonEntity, Integer> {
+	
+	//Methode renvoyer dans une collection
+	public List<PokemonEntity> findByNom(String nomPokemon);
 
 }
