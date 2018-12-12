afb8402284b7f869033a13853e751e2a1ed84746
Administrateur <christinebelle.pereira@gmail.com>
2018-12-12 15:14:31 +0100

    class Entity pokemon et type

--- src/main/java/projetPokemon/model/PokemonEntity.java
+++ src/main/java/projetPokemon/model/PokemonEntity.java
@@ -1,17 +1,158 @@
 package projetPokemon.model;
 
-public class PokemonEntity {
+/**
+ * @author Christine
+ * Table de la base de donnée contenant le nom des pokemons et leurs évolutions
+ */
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class PokemonEntity implements Serializable {
 	
-    @Id
+    /**
+	 * numero de version par default de la classe pour que les ojets generes soit reconnu
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer idPok;
-    private String nomPok;
-    private String evol1;
-    private String evol2;
+    private Integer idPokemon;
+    private String nomPokemon;
+    private String nomEvol1;
+    private String nomEvol2;
     
-    @OneToMany(cascade=CascadeType.ALL)
-    private Type type;
+    /**
+     * relation de la clef etrangere de la table
+     * mappedBy="idType"
+     */
+    @OneToMany(mappedBy="idType", cascade=CascadeType.REMOVE)
+    private Set<TypeEntity> types = new HashSet<TypeEntity>();
     
-    
+    /**
+     * constructeur vide
+     */
+	public PokemonEntity() {
+		
+	}
+
+	/**
+	 * get et set methode
+	 */
+	public Integer getIdPokemon() {
+		return idPokemon;
+	}
+
+	public void setIdPokemon(Integer idPokemon) {
+		this.idPokemon = idPokemon;
+	}
+
+	public String getNomPokemon() {
+		return nomPokemon;
+	}
+
+	public void setNomPokemon(String nomPokemon) {
+		this.nomPokemon = nomPokemon;
+	}
+
+	public String getNomEvol1() {
+		return nomEvol1;
+	}
+
+	public void setNomEvol1(String nomEvol1) {
+		this.nomEvol1 = nomEvol1;
+	}
+
+	public String getNomEvol2() {
+		return nomEvol2;
+	}
+
+	public void setNomEvol2(String nomEvol2) {
+		this.nomEvol2 = nomEvol2;
+	}
+
+	public Set<TypeEntity> getTypes() {
+		return types;
+	}
+
+	public void setTypes(Set<TypeEntity> types) {
+		this.types = types;
+	}
+	
+
+	/**
+	 * hashCode (stocke l'adresse memoire de la recherche) 
+	 * et equals (pour comparer deux valeurs) methode
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idPokemon == null) ? 0 : idPokemon.hashCode());
+		result = prime * result + ((nomEvol1 == null) ? 0 : nomEvol1.hashCode());
+		result = prime * result + ((nomEvol2 == null) ? 0 : nomEvol2.hashCode());
+		result = prime * result + ((nomPokemon == null) ? 0 : nomPokemon.hashCode());
+		result = prime * result + ((types == null) ? 0 : types.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PokemonEntity other = (PokemonEntity) obj;
+		if (idPokemon == null) {
+			if (other.idPokemon != null)
+				return false;
+		} else if (!idPokemon.equals(other.idPokemon))
+			return false;
+		if (nomEvol1 == null) {
+			if (other.nomEvol1 != null)
+				return false;
+		} else if (!nomEvol1.equals(other.nomEvol1))
+			return false;
+		if (nomEvol2 == null) {
+			if (other.nomEvol2 != null)
+				return false;
+		} else if (!nomEvol2.equals(other.nomEvol2))
+			return false;
+		if (nomPokemon == null) {
+			if (other.nomPokemon != null)
+				return false;
+		} else if (!nomPokemon.equals(other.nomPokemon))
+			return false;
+		if (types == null) {
+			if (other.types != null)
+				return false;
+		} else if (!types.equals(other.types))
+			return false;
+		return true;
+	}
+
+	
+	/**
+	 * toString methode pour afficher les variables
+	 */
+	@Override
+	public String toString() {
+		return "PokemonEntity [idPokemon=" + idPokemon + ", nomPokemon=" + nomPokemon + ", nomEvol1=" + nomEvol1
+				+ ", nomEvol2=" + nomEvol2 + ", types=" + types + "]";
+	}
+
+	
+	 
 
 }
--- src/main/java/projetPokemon/model/TypeEntity.java
+++ src/main/java/projetPokemon/model/TypeEntity.java
@@ -1,5 +1,194 @@
 package projetPokemon.model;
 
-public class TypeEntity {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class TypeEntity implements Serializable{
+
+	/**
+	 * numero de version par default de la classe pour que les ojets generes soit reconnu
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer idType;
+	private String nomType;
+	private String attaqueIm1;
+	private String attaqueIm2;
+	private String attaqueChar1;
+	private String attaqueChar2;
+	
+	/**
+     * relation de la clef etrangere de la table
+     * name ="idPokemon"
+     */
+	@ManyToOne
+	@JoinColumn(name = "idPokemon")
+	private PokemonEntity pokemons;
+
+	
+	/**
+     * constructeur vide
+     */
+	public TypeEntity() {
+		
+	}
+
+	/**
+	 * get et set methode
+	 */
+	public Integer getIdType() {
+		return idType;
+	}
+
+
+	public void setIdType(Integer idType) {
+		this.idType = idType;
+	}
+
+
+	public String getNomType() {
+		return nomType;
+	}
+
+
+	public void setNomType(String nomType) {
+		this.nomType = nomType;
+	}
+
+
+	public String getAttaqueIm1() {
+		return attaqueIm1;
+	}
+
+
+	public void setAttaqueIm1(String attaqueIm1) {
+		this.attaqueIm1 = attaqueIm1;
+	}
+
+
+	public String getAttaqueIm2() {
+		return attaqueIm2;
+	}
+
+
+	public void setAttaqueIm2(String attaqueIm2) {
+		this.attaqueIm2 = attaqueIm2;
+	}
+
+
+	public String getAttaqueChar1() {
+		return attaqueChar1;
+	}
+
+
+	public void setAttaqueChar1(String attaqueChar1) {
+		this.attaqueChar1 = attaqueChar1;
+	}
+
+
+	public String getAttaqueChar2() {
+		return attaqueChar2;
+	}
+
+
+	public void setAttaqueChar2(String attaqueChar2) {
+		this.attaqueChar2 = attaqueChar2;
+	}
+
+
+	public PokemonEntity getPokemons() {
+		return pokemons;
+	}
+
+
+	public void setPokemons(PokemonEntity pokemons) {
+		this.pokemons = pokemons;
+	}
+
+	/**
+	 * hashCode (stocke l'adresse memoire de la recherche) 
+	 * et equals (pour comparer deux valeurs) methode
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((attaqueChar1 == null) ? 0 : attaqueChar1.hashCode());
+		result = prime * result + ((attaqueChar2 == null) ? 0 : attaqueChar2.hashCode());
+		result = prime * result + ((attaqueIm1 == null) ? 0 : attaqueIm1.hashCode());
+		result = prime * result + ((attaqueIm2 == null) ? 0 : attaqueIm2.hashCode());
+		result = prime * result + ((idType == null) ? 0 : idType.hashCode());
+		result = prime * result + ((nomType == null) ? 0 : nomType.hashCode());
+		result = prime * result + ((pokemons == null) ? 0 : pokemons.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TypeEntity other = (TypeEntity) obj;
+		if (attaqueChar1 == null) {
+			if (other.attaqueChar1 != null)
+				return false;
+		} else if (!attaqueChar1.equals(other.attaqueChar1))
+			return false;
+		if (attaqueChar2 == null) {
+			if (other.attaqueChar2 != null)
+				return false;
+		} else if (!attaqueChar2.equals(other.attaqueChar2))
+			return false;
+		if (attaqueIm1 == null) {
+			if (other.attaqueIm1 != null)
+				return false;
+		} else if (!attaqueIm1.equals(other.attaqueIm1))
+			return false;
+		if (attaqueIm2 == null) {
+			if (other.attaqueIm2 != null)
+				return false;
+		} else if (!attaqueIm2.equals(other.attaqueIm2))
+			return false;
+		if (idType == null) {
+			if (other.idType != null)
+				return false;
+		} else if (!idType.equals(other.idType))
+			return false;
+		if (nomType == null) {
+			if (other.nomType != null)
+				return false;
+		} else if (!nomType.equals(other.nomType))
+			return false;
+		if (pokemons == null) {
+			if (other.pokemons != null)
+				return false;
+		} else if (!pokemons.equals(other.pokemons))
+			return false;
+		return true;
+	}
+
+	/**
+	 * toString methode pour afficher les variables
+	 */
+	@Override
+	public String toString() {
+		return "TypeEntity [idType=" + idType + ", nomType=" + nomType + ", attaqueIm1=" + attaqueIm1 + ", attaqueIm2="
+				+ attaqueIm2 + ", attaqueChar1=" + attaqueChar1 + ", attaqueChar2=" + attaqueChar2 + ", pokemons="
+				+ pokemons + "]";
+	}
+	
+	
 }
